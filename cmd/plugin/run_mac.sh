93c04816ee1935f22a639082c15f4a4576720427
ade <ade.li.indie@gmail.com>
2019-05-01 12:25:07 +0800

    mac run ok

---
+++ cmd/plugin/run_mac.sh
@@ -0,0 +1,1 @@
+cp ../../prebuild/liblua.dylib .
--- lib/make_osx_lua53.sh
+++ lib/make_osx_lua53.sh
@@ -2,9 +2,15 @@ mkdir -p build_osx && cd build_osx
 cmake -GXcode ../
 cd ..
 cmake --build build_osx --config Release
+
+install_name_tool -id "@executable_path/liblua.dylib" build_osx/Release/liblua.dylib
+
 mkdir -p release/osx/
 cp build_osx/Release/lua release/osx/
 cp build_osx/Release/luac release/osx/
 cp build_osx/Release/liblua.dylib release/osx/
+
 # copy to your working dir
-cp build_osx/Release/liblua.dylib ../cmd/lua/
+#cp build_osx/Release/liblua.dylib ../cmd/lua/
+cp build_osx/Release/liblua.dylib ../prebuild
+
