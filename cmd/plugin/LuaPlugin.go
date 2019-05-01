0ee3e02ec961529cefa7843ce6464acbc9580ba5
Gitea <gitea@fake.local>
2019-04-30 23:59:58 -0400

    linux run ok

--- cmd/plugin/LuaPlugin.go
+++ cmd/plugin/LuaPlugin.go
@@ -1,7 +1,7 @@
 package main
 /*
 #cgo CFLAGS: -Iinclude
-#cgo LDFLAGS: -L../../prebuild -llua
+#cgo LDFLAGS: -L../../prebuild -llua -lm
 #ifndef __glua_h__
 #define __glua_h__
 #include <stdlib.h> // C.free
